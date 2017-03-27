c1a75fb38556978e0968c10f00182bab0d867d41
Jörgen <kejsardamberg@hotmail.com>
2017-03-27 09:33:15 +0200

    Added description

--- original/src/test/java/se/claremont/tests/HomePageTest.java
+++ original/src/test/java/se/claremont/tests/HomePageTest.java
@@ -1,29 +1,57 @@
 package se.claremont.tests;
 
-import org.junit.*;
-import org.junit.rules.TestName;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import se.claremont.autotest.common.testrun.Settings;
 import se.claremont.autotest.common.testrun.TestRun;
 import se.claremont.autotest.common.testset.TestSet;
 
 
 /**
- * Created by jordam on 2016-12-04.
+ * When running these tests logs will be compiled to HTML and plain text and
+ * stored in a folder called TAF in your user home directory.
+ *
+ * The tests open the Claremont home page and performs a few verifications.
+ *
+ * Feel free to explore test run settings (TestRun.setSettingsValue), the
+ * command line interface, logging possibilities (there are currently three
+ * test case report types (Plain text, console output, and HTML report),
+ * that can be combined.
+ *
+ * There are also five types of test run reports:
+ *  > Console output
+ *  > HTML test run summary file report
+ *  > Emailed html report
+ *  > TAF Backend Server integration
+ *  > propagation of test results to Testlink open source test management system.
+ *
+ * Technologies supported includes:
+ *  > Web (with Selenium supported browsers)
+ *  > REST (with both JSON and XML)
+ *  > Java (through jar files or Java Web Start)
+ *
+ *  Test levels supported are:
+ *  > System testing or system integration testing (with class extension TestSet)
+ *  > Unit testing (with class extension UnitTestClass)
+ *
  */
 public class HomePageTest extends TestSet {
     private TestActions app;
 
     @BeforeClass
-    public static void classSetup(){
+    public static void classSetup() {
         TestRun.setSettingsValue(Settings.SettingParameters.EMAIL_REPORT_RECIPIENTS_COMMA_SEPARATED_LIST_OF_ADDRESSES, "no.no@no.no");
         TestRun.setSettingsValue(Settings.SettingParameters.EMAIL_SERVER_ADDRESS, "smtp.google.com");
         TestRun.setSettingsValue(Settings.SettingParameters.EMAIL_SENDER_ADDRESS, "mailrelay@gmail.com");
         TestRun.setSettingsValue(Settings.SettingParameters.EMAIL_SERVER_PORT, "25");
         TestRun.setSettingsValue(Settings.SettingParameters.EMAIL_SMTP_OR_GMAIL, "SMTP");
-        TestRun.setSettingsValue(Settings.SettingParameters.PATH_TO_LOGO, "http://46.101.193.212/TAF/images/claremontlogo.gif");
 
         //Base log folder is a TAF folder under user home folder
         //TestRun.setSettingsValue(Settings.SettingParameters.BASE_LOG_FOLDER, "C:\\TEMP\\");
+        //TestRun.setSettingsValue(Settings.SettingParameters.URL_TO_TAF_BACKEND, "http://127.0.0.1:81/taf");
+        //TestRun.setSettingsValue(Settings.SettingParameters.URL_TO_TESTLINK_ADAPTER, "http://127.0.0.1:2222/taftestlinkadapter");
     }
 
     @Before
@@ -46,7 +74,12 @@ public class HomePageTest extends TestSet {
         app.checkLandingPageLinks();
         app.checkLandingPageLayout();
         app.checkLandingPageTexts();
+
+        //The command below send the current HTML source of the web page for validation with W3C
         //app.checkLandingPageWithW3CValidator();
+
+        //The command below identifies all links on current web page, and report any broken links.
+        //app.web.reportBrokenLinks();
     }
 
 
